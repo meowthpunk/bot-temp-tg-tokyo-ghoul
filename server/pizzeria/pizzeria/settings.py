a7064ca2b17fa638c2b1cfb08f1e2a60d70d354c
= <=>
2022-04-24 23:01:46 +1000

    Minor changes

--- server/pizzeria/pizzeria/settings.py
+++ server/pizzeria/pizzeria/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pizzeria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER' : 'django',
+        'PASSWORD' : 'AJ+Qa%pzkdDAR3TF',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
